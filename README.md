7cda0c33ab481d20e7f4b2bab608b3c8ec60fb89
Rohit-R-Sahu <rohitrsahu2000@gmail.com>
2024-01-23 19:20:26 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+# How to Run
+Step -1 : cd ToolkitOnHover 
+Step -2 : npm install
+Step -3 : npm run dev
+step- 4 : Open Browser and visit http://localhost:5173/
+
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"; //imported useState Hook
+import "./App.css"; //imported to add styles
+import { Tooltip } from "react-tooltip"; //npm package to manage tooltip
+
+function App() {
+  //created a state to update position 
+  const [place, setPlace] = useState("top");
+
+  //created a function to update state as per position input from user
+  function updateData(n) {
+    let p = n.target.value;
+    setPlace(p);
+  }
+  return (
+    <div>
+      {/* Created a label for option menu to select position of tooltop */}
+      <label htmlFor="place">Select a Position : </label>
+      {/* Added different position using select tag and used onchange attribute to take input of position from user and update state using function updateData() */}
+      <select name="place" id="place" onChange={updateData}>
+        <option value={"top"}>Top</option>
+        <option value={"right"}>Right</option>
+        <option value={"left"}>Left</option>
+        <option value={"bottom"}>Bottom</option>
+      </select>
+      {/* used data-tooltip-id to pass id to tooltip component and data-tooltip-content to add component */}
+      <h2
+        data-tooltip-id={"my-tooltip"}
+        data-tooltip-content={"Hello World..!!!"}
+      >
+        Hover over me
+      </h2>
+      {/* added Tooltip component and added place attribute along with state to update the postion of tooltip and added some inline styling*/}
+      <Tooltip
+        id="my-tooltip"
+        style={{ backgroundColor: "white", color: "#000" }}
+        place={place}
+      />
+    </div>
+  );
+}
+
+export default App;
